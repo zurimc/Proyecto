104eeed10a4ae0fef31241446fe080b4bce694a8
LabVision01 <LabVision01@mini-de-aldo>
2019-07-04 19:14:55 -0500

    kdkeror

--- StimSD/src/main/java/com/lab/sdt/service/EstadoUnidad.java
+++ StimSD/src/main/java/com/lab/sdt/service/EstadoUnidad.java
@@ -1,6 +1,12 @@
 package com.lab.sdt.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.faces.model.SelectItem;
+import javax.faces.model.SelectItemGroup;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +15,9 @@ import org.springframework.transaction.annotation.Transactional;
 import com.lab.sdt.dao.EstadoMapper;
 import com.lab.sdt.dto.EstadoDTO;
 import com.lab.sdt.model.Estado;
+import com.lab.sdt.model.EstadoExample;
+
+import com.lab.sdt.model.EstadoExample.Criteria;
 
 
 
@@ -19,8 +28,24 @@ public class EstadoUnidad {
 	@Autowired
 	private EstadoMapper estadoMapper;
 	
-	public List<Estado> selectEstado_v(){
-
-		return estadoMapper.selectEstado();
+	public List<String> selectEstado_v(){
+		SelectItemGroup g1 = new SelectItemGroup("estado");
+		List<Estado> todos_los_estados  = new ArrayList<Estado>();
+		List<String> listaestados = new ArrayList<String>();
+		/*listaestados.add(""+0);
+		listaestados.add(""+1);
+		listaestados.add(estadoMapper.selectByPrimaryKey(2).getEstado());
+		listaestados.add(""+3);
+		listaestados.add(""+4);
+		*/
+		for(int ii = 1; ii<= 33; ii++) {
+			//estadoMapper.selectByPrimaryKey(2).getEstado()
+		//listaestados.add(""+0);
+			listaestados.add(estadoMapper.selectByPrimaryKey(ii).getEstado());
+		}
+		return listaestados;
 	}
+	
+
+	
 }
