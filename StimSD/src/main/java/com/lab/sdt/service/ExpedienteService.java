12e6b1a19c3c0c4b4392868562dab59928e99282
LabVision01 <LabVision01@192.168.0.103>
2019-08-16 14:50:53 -0500

    formatos

--- StimSD/src/main/java/com/lab/sdt/service/ExpedienteService.java
+++ StimSD/src/main/java/com/lab/sdt/service/ExpedienteService.java
@@ -1,5 +1,13 @@
 package com.lab.sdt.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class ExpedienteService {
 
+	
+	@Autowired
 }
