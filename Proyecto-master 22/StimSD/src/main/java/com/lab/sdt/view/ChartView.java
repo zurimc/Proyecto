99f69eddb749d09393126e2d20fcfd83e3287975
LabVision01 <LabVision01@192.168.0.129>
2019-10-16 16:14:39 -0500

    png

---
+++ Proyecto-master 22/StimSD/src/main/java/com/lab/sdt/view/ChartView.java
@@ -0,0 +1,245 @@
+package com.lab.sdt.view;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.ItemSelectEvent;
+import org.primefaces.model.DashboardModel;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.CartesianChartModel;
+import org.primefaces.model.chart.CategoryAxis;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.DateAxis;
+import org.primefaces.model.chart.LineChartModel;
+import org.primefaces.model.chart.LineChartSeries;
+import org.primefaces.model.diagram.DiagramModel;
+
+@ManagedBean
+public class ChartView implements Serializable {
+	private LineChartModel lineModel1;
+    private LineChartModel lineModel2;
+    private LineChartModel zoomModel;
+  
+    private LineChartModel areaModel;
+  
+    
+    private LineChartModel animatedModel1;
+  
+    private LineChartModel multiAxisModel;
+    private LineChartModel dateModel;
+ 
+    @PostConstruct
+    public void init() {
+        createLineModels();
+        createAreaModel();
+       
+       
+      
+        createDateModel();
+    }
+ 
+    public void itemSelect(ItemSelectEvent event) {
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Item selected",
+                "Item Index: " + event.getItemIndex() + ", Series Index:" + event.getSeriesIndex());
+ 
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+ 
+    public LineChartModel getLineModel1() {
+        return lineModel1;
+    }
+ 
+    public LineChartModel getLineModel2() {
+        return lineModel2;
+    }
+ 
+    public LineChartModel getZoomModel() {
+        return zoomModel;
+    }
+ 
+  
+ 
+    public CartesianChartModel getAreaModel() {
+        return areaModel;
+    }
+ 
+   
+ 
+    public LineChartModel getAnimatedModel1() {
+        return animatedModel1;
+    }
+ 
+   
+    public LineChartModel getMultiAxisModel() {
+        return multiAxisModel;
+    }
+ 
+    public LineChartModel getDateModel() {
+        return dateModel;
+    }
+ 
+    
+    private LineChartModel initCategoryModel() {
+        LineChartModel model = new LineChartModel();
+ 
+        ChartSeries boys = new ChartSeries();
+        boys.setLabel("Boys");
+        boys.set("2004", 120);
+        boys.set("2005", 100);
+        boys.set("2006", 44);
+        boys.set("2007", 150);
+        boys.set("2008", 25);
+ 
+        ChartSeries girls = new ChartSeries();
+        girls.setLabel("Girls");
+        girls.set("2004", 52);
+        girls.set("2005", 60);
+        girls.set("2006", 110);
+        girls.set("2007", 90);
+        girls.set("2008", 120);
+ 
+        model.addSeries(boys);
+        model.addSeries(girls);
+ 
+        return model;
+    }
+ 
+    private void createLineModels() {
+        lineModel1 = initLinearModel();
+        lineModel1.setTitle("Linear Chart");
+        lineModel1.setLegendPosition("e");
+        Axis yAxis = lineModel1.getAxis(AxisType.Y);
+        yAxis.setMin(0);
+        yAxis.setMax(10);
+ 
+        lineModel2 = initCategoryModel();
+        lineModel2.setTitle("Category Chart");
+        lineModel2.setLegendPosition("e");
+        lineModel2.setShowPointLabels(true);
+        lineModel2.getAxes().put(AxisType.X, new CategoryAxis("Years"));
+        yAxis = lineModel2.getAxis(AxisType.Y);
+        yAxis.setLabel("Births");
+        yAxis.setMin(0);
+        yAxis.setMax(200);
+ 
+        zoomModel = initLinearModel();
+        zoomModel.setTitle("Zoom");
+        zoomModel.setZoom(true);
+        zoomModel.setLegendPosition("e");
+        yAxis = zoomModel.getAxis(AxisType.Y);
+        yAxis.setMin(0);
+        yAxis.setMax(10);
+    }
+ 
+    private void createAreaModel() {
+        areaModel = new LineChartModel();
+ 
+        LineChartSeries boys = new LineChartSeries();
+        boys.setFill(true);
+        boys.setLabel("Boys");
+        boys.set("2004", 120);
+        boys.set("2005", 100);
+        boys.set("2006", 44);
+        boys.set("2007", 150);
+        boys.set("2008", 25);
+ 
+        LineChartSeries girls = new LineChartSeries();
+        girls.setFill(true);
+        girls.setLabel("Girls");
+        girls.set("2004", 52);
+        girls.set("2005", 60);
+        girls.set("2006", 110);
+        girls.set("2007", 90);
+        girls.set("2008", 120);
+ 
+        areaModel.addSeries(boys);
+        areaModel.addSeries(girls);
+ 
+        areaModel.setTitle("Area Chart");
+        areaModel.setLegendPosition("ne");
+        areaModel.setStacked(true);
+        areaModel.setShowPointLabels(true);
+ 
+        Axis xAxis = new CategoryAxis("Years");
+        areaModel.getAxes().put(AxisType.X, xAxis);
+        Axis yAxis = areaModel.getAxis(AxisType.Y);
+        yAxis.setLabel("Births");
+        yAxis.setMin(0);
+        yAxis.setMax(300);
+    }
+ 
+  
+    
+   
+    private LineChartModel initLinearModel() {
+        LineChartModel model = new LineChartModel();
+ 
+        LineChartSeries series1 = new LineChartSeries();
+        series1.setLabel("Series 1");
+ 
+        series1.set(1, 2);
+        series1.set(2, 1);
+        series1.set(3, 3);
+        series1.set(4, 6);
+        series1.set(5, 8);
+ 
+        LineChartSeries series2 = new LineChartSeries();
+        series2.setLabel("Series 2");
+ 
+        series2.set(1, 6);
+        series2.set(2, 3);
+        series2.set(3, 2);
+        series2.set(4, 7);
+        series2.set(5, 9);
+ 
+        model.addSeries(series1);
+        model.addSeries(series2);
+ 
+        return model;
+    }
+ 
+ 
+
+ 
+    private void createDateModel() {
+        dateModel = new LineChartModel();
+        LineChartSeries series1 = new LineChartSeries();
+        series1.setLabel("Series 1");
+ 
+        series1.set("2014-01-01", 51);
+        series1.set("2014-01-06", 22);
+        series1.set("2014-01-12", 65);
+        series1.set("2014-01-18", 74);
+        series1.set("2014-01-24", 24);
+        series1.set("2014-01-30", 51);
+ 
+        LineChartSeries series2 = new LineChartSeries();
+        series2.setLabel("Series 2");
+ 
+        series2.set("2014-01-01", 32);
+        series2.set("2014-01-06", 73);
+        series2.set("2014-01-12", 24);
+        series2.set("2014-01-18", 12);
+        series2.set("2014-01-24", 74);
+        series2.set("2014-01-30", 62);
+ 
+        dateModel.addSeries(series1);
+        dateModel.addSeries(series2);
+ 
+        dateModel.setTitle("Zoom for Details");
+        dateModel.setZoom(true);
+        dateModel.getAxis(AxisType.Y).setLabel("Values");
+        DateAxis axis = new DateAxis("Dates");
+        axis.setTickAngle(-50);
+        axis.setMax("2014-02-01");
+        axis.setTickFormat("%b %#d, %y");
+ 
+        dateModel.getAxes().put(AxisType.X, axis);
+    }
+    
+}
